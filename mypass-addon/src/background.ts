c6c13bdac752fba04fd19381a22481683e21be74
Yukkku <yukku.scratcher@gmail.com>
2026-08-13 17:13:14 +0900

    ちゃんとinitが終わってから読み込むように

--- mypass-addon/src/background.ts
+++ mypass-addon/src/background.ts
@@ -1,16 +1,16 @@
 import init, { generate } from "mypass-wasm";
 import wasm from "mypass-wasm/mypass_wasm_bg.wasm";
 
-init(wasm);
+init(wasm).then(() => {
+  browser.contextMenus.create({
+    id: 'req-mypass',
+    title: 'mypass',
+    contexts: ['editable'],
+  });
 
-browser.contextMenus.create({
-  id: 'req-mypass',
-  title: 'mypass',
-  contexts: ['editable'],
-});
-
-browser.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId !== 'req-mypass') return;
-  if (tab?.id == null) return;
-  browser.tabs.sendMessage(tab.id, generate({ len: 100 }, "scratch", new Uint8Array()));
+  browser.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId !== 'req-mypass') return;
+    if (tab?.id == null) return;
+    browser.tabs.sendMessage(tab.id, generate({ len: 100 }, "scratch", new Uint8Array()));
+  });
 });
