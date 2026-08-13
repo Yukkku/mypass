c54a52211b3f9175e85ef158ca5ee2fcbea5eb22
Yukkku <yukku.scratcher@gmail.com>
2026-08-13 18:34:07 +0900

    入力, execCommandの代替が無いのでこまった

--- mypass-addon/src/content.ts
+++ mypass-addon/src/content.ts
@@ -2,8 +2,6 @@ const manifest = browser.runtime.getManifest();
 console.log(`${manifest.name} ${manifest.version}`);
 
 browser.runtime.onMessage.addListener(pass => {
-  const elem = document.activeElement;
-  console.log(elem);
-  if (!(elem instanceof HTMLInputElement)) return;
-  elem.value = pass;
+  // execCommandは非推奨らしいけど, 代替手段が無いんだからしょうがない
+  document.execCommand("insertText", false, pass);
 });
