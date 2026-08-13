750c4fde798d3d8ca9779ef47a8cc7ebf0714af8
Yukkku <yukku.scratcher@gmail.com>
2026-08-13 19:37:52 +0900

    設定ファイルも読ませよう

--- mypass-addon/src/option.ts
+++ mypass-addon/src/option.ts
@@ -18,4 +18,22 @@ const masterpass = (() => {
   return label;
 })();
 
-document.body.appendChild(masterpass);
+const config = (() => {
+  const input = document.createElement("input");
+  input.type = "file";
+  input.style.marginLeft = "0.5em";
+
+  input.addEventListener("change", async () => {
+    if (input.files!.length == 0) return;
+    const file = input.files![0]!;
+    const config = JSON.parse(await file.text());
+    browser.storage.local.set({ config });
+  });
+
+  const label = document.createElement("label");
+  label.textContent = "Config:";
+  label.appendChild(input);
+  return label;
+})();
+
+document.body.append(masterpass, config);
